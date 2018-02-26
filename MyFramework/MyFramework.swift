4d5ba1af6484d7c1312bef5970c0d7f33228d2cb
Mohamed Abdul-Hameed <mhd.b.abdulhameed@gmail.com>
2018-02-26 17:18:21 +0200

    The framework is created and its .podspec file is created and edited.

---
+++ MyFramework/MyFramework.swift
@@ -0,0 +1,14 @@
+//
+//  MyFramework.swift
+//  MyFramework
+//
+//  Created by Mohamed Abdul-Hameed on 2/26/18.
+//  Copyright © 2018 Mohamed Abdul-Hameed. All rights reserved.
+//
+
+import Foundation
+
+public class MyFramework {
+    static let shared = MyFramework()
+    func foo() {}
+}
